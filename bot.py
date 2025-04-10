5641f311be78b5881d0a6eff3d01e51ee8c16770
raquelpacheco007 <raquel.p.pereira@gmail.com>
2025-04-10 18:00:22 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -522,8 +522,8 @@ async def analisar_pronuncia(transcricao, audio_path, nivel):
         4. Dica prática para melhorar, coloque: Dica prática para melhorar
         IMPORTANTE: Dê suas explicações em português para facilitar o entendimento do aluno, mas não traduza as frases da transcrição para português, mantenha em inglês.
         Frase dita pelo aluno (use como base para análise, **não repita na resposta**): "{transcricao}"
-        """
-        Transcrição: {transcricao}"""
+    
+      """
     )
     
     resposta = openai_client.chat.completions.create(
