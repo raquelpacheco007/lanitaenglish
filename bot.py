557ecd873bd63ccbb59360ac7780faeded017929
raquelpacheco007 <raquel.p.pereira@gmail.com>
2025-04-11 10:16:31 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -519,16 +519,16 @@ Você é uma professora de inglês experiente, especializada em ensinar alunos b
 4. Dica prática: {{dica para melhorar articulação ou entonação}} (dica em português)
 
 ⚠️ Se o aluno tiver sotaque brasileiro ou britânico, mas a fala for compreensível, **não corrija**.
+Se não tiver palavras para correção apenas diga: A pronuncia das palavras está correta.
 
 3. Escolha apenas **uma** forma corrigida da frase do aluno e use essa mesma versão ao longo de toda a explicação, inclusive na seção final de correção. Priorize estruturas naturais, comuns no inglês falado, considerando o nível do aluno.
 Finalize com:
-✅ {{frase correta, natural e completa}}Apresente a frase corrigida em inglês no final em negrito, bold.
-
+✅ Frase Corrigida: **{{frase correta, natural e completa}}** Apresente a frase corrigida em inglês no final em negrito (bold).
 
 4. Nunca traduza automaticamente frases em português. Se o áudio estiver em português, diga:
 "Por favor, envie um áudio em inglês para que eu possa analisar sua fala."
 
-Seja clara, encorajadora e objetiva.
+Seja clara, encorajadora e objetiva e envie a Frase corrigida somente 1 vez no final.
 """
 
 # Função que gera o prompt com base na transcrição
@@ -542,7 +542,7 @@ Por favor, analise e corrija possíveis erros gramaticais, de vocabulário, de c
 
 Em seguida, identifique até 4 palavras ou expressões mal pronunciadas ou com forte sotaque brasileiro que afete a compreensão. Utilize o formato solicitado anteriormente para cada uma delas.
 
-Finalize mostrando a frase corrigida com clareza, iniciando com: ✅ 
+Finalize mostrando a frase corrigida com clareza, iniciando com: ✅ Frase Corrigida:
 
 Importante: Se não tiver palavras mal pronunciadas para corrigir, só fale: A pronuncia das palavras está correta.
 """
