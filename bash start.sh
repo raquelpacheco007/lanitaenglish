2cfa64f0adc17c42a5d78442732b949a6912a91a
raquelpacheco007 <raquel.p.pereira@gmail.com>
2025-04-16 14:26:19 -0300

    Update bash start.sh

--- bash start.sh
+++ bash start.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Mata qualquer processo Python que possa estar rodando
-pkill -f python || true
-
-# Espera um pouco para garantir que os processos foram encerrados
-sleep 2
-
-# Inicia o servidor Flask
-python server.py
+python bot.py
